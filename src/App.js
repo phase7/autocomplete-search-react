b8d0202ba9c510b805ff1cf6d5dedce77b87ca70
P7 <ratulroy20@gmail.com>
2020-08-04 19:14:29 +0600

    Uncontrolled form is set up and working. We can see suggestions now!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import AutoCompleteText from './component/AutoCompleteText'
+import AutoCompleteText from './component/AutoCompleteText';
 
 function App() {
   return (
     <div className="App">
-      <AutoCompleteText/>      
+      <AutoCompleteText />      
     </div>
   );
 }
--- src/component/AutoCompleteText.js
+++ src/component/AutoCompleteText.js
@@ -11,18 +11,45 @@ export default class AutoCompleteText extends React.Component {
 			'Sara',
 			'Salim',
 			'Jane',
-			'Janet'
+			'Janet',
 		];
+		this.state = {
+			suggestion: [],
+		};
+
 	}
+
+		onTextChange = (e) => {
+			const value = e.target.value;
+			let suggestion = [];
+			if (0 < value.length){
+				const regex = new RegExp(`^${value}`, 'i');
+				suggestion = this.items.sort().filter(v => regex.test(v))
+			}
+			this.setState({suggestion});
+			}
+
+		renderSuggestion() {
+			const { suggestion } = this.state;
+
+			if (suggestion.length === 0){
+				return (<div></div>);
+			}
+			return(
+				<ul>
+					{suggestion.map((item) => <li>{item}</li>)}
+				</ul>
+
+				);
+		}
+	
 
 	render(){
 		return(
 			<div>
-				<input type="text" />
-				<ul>
-					{this.items.map((item) => <li>{item}</li>)}
-				</ul>
+				<input onChange={this.onTextChange} type="text" />
+				{this.renderSuggestion()}
 			</div>
-			)
+			);
 	}
 }
