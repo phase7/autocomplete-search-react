fe3aedf083b4ebb44b482b05cbd4b79cf13d1978
P7 <ratulroy20@gmail.com>
2020-08-05 00:42:15 +0600

    Updates

---
+++ precache-manifest.307d027800c99a991b8fb04623d70ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5ceb92c330aa0d181e3a78a6ef49e5",
+    "url": "/autocomplete-search-react/index.html"
+  },
+  {
+    "revision": "8c5a8b2f988ebf8c58b6",
+    "url": "/autocomplete-search-react/static/css/main.fc542b7f.chunk.css"
+  },
+  {
+    "revision": "f0531c2ad8c0635e4428",
+    "url": "/autocomplete-search-react/static/js/2.c45239e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/autocomplete-search-react/static/js/2.c45239e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5a8b2f988ebf8c58b6",
+    "url": "/autocomplete-search-react/static/js/main.33906038.chunk.js"
+  },
+  {
+    "revision": "7fa299e1c9ef9c4f9841",
+    "url": "/autocomplete-search-react/static/js/runtime-main.470bd67e.js"
+  }
+]);
