06ab23b48ac1ad6fcef0deeacc026365f093f74e
P7 <ratulroy20@gmail.com>
2020-08-04 22:32:17 +0600

    added another simillar search box NBA MVP list is added and passed as props just like countries

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import './AutoCompleteText.css';
 import AutoCompleteText from './component/AutoCompleteText';
 import countries from './countries';
+import nba from './nba';
 
 function App() {
   return (
     <div className="App">
-    <h3>Countries</h3>
+      <h3>Countries</h3>
       <AutoCompleteText items={countries} />      
+      <h3>NBA MVPs</h3>
+      <AutoCompleteText items={nba} />      
     </div>
   );
 }
